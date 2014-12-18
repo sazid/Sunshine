e413f21cd246fcf390cb4583084e32f28cf2c4cb
Mohammed Sazid-Al-Rashid <sazidz@live.com>
2014-12-19 03:07:48 +0600

    Add initial code for getting data from the internet

--- Sunshine/app/src/main/java/com/mohammedsazidalrashid/android/sunshine/MainActivity.java
+++ Sunshine/app/src/main/java/com/mohammedsazidalrashid/android/sunshine/MainActivity.java
@@ -28,6 +28,7 @@ package com.mohammedsazidalrashid.android.sunshine;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -36,6 +37,12 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -89,6 +96,63 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            // These two need to be declared outside of the try/catch so that they can be closed
+            // in the finally block
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as String
+            String forecastJsonStr = null;
+
+            try {
+                // Construct the url
+                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast" +
+                        "/daily?q=Dhaka,bd&mode=json&units=metric&cnt=7");
+
+                // Open the connection, set its request method and connect to the remote system
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                StringBuffer buffer = new StringBuffer();
+                InputStream inputStream = urlConnection.getInputStream();
+                if (inputStream == null) {
+                    // Nothing to do
+                    forecastJsonStr = null;
+                } else {
+                    reader = new BufferedReader(new InputStreamReader(inputStream));
+                }
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Adding "\n" for debugging purposes/readability
+                    buffer.append(line).append("\n");
+                }
+
+                if (buffer.length() == 0) {
+                    forecastJsonStr = null;
+                }
+                forecastJsonStr = buffer.toString();
+                Log.i("PlaceholderFragment", "JSON: " + forecastJsonStr);
+            } catch (IOException exception) {
+                Log.e("PlaceholderFragment", "Error ", exception);
+            } catch (NullPointerException exception) {
+                Log.e("PlaceholderFragment", "Error ", exception);
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException exception) {
+                        Log.e("PlaceholderFragment", "Error ", exception);
+                    }
+                }
+            }
 
             String[] forecastArray = {
                     "Today - Sunny - 88/63",
