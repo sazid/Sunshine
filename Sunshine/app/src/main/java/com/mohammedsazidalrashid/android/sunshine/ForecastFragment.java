f969dab9889503ecbf5fd15a22edaab49daf581a
Mohammed Sazid-Al-Rashid <sazidz@live.com>
2014-12-19 21:39:12 +0600

    Reformat networking code

---
+++ Sunshine/app/src/main/java/com/mohammedsazidalrashid/android/sunshine/ForecastFragment.java
@@ -0,0 +1,154 @@
+/**********************************************************************************
+ * Bismillahir Rahmanir Rahim, ALLAHU AKBAR                                       *
+ * The MIT License (MIT)                                                          *
+ *                                                                                *
+ * Copyright (c) 2014 Mohammed Sazid-Al-Rashid                                    *
+ *                                                                                *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy   *
+ * of this software and associated documentation files (the "Software"), to deal  *
+ * in the Software without restriction, including without limitation the rights   *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      *
+ * copies of the Software, and to permit persons to whom the Software is          *
+ * furnished to do so, subject to the following conditions:                       *
+ *                                                                                *
+ * The above copyright notice and this permission notice shall be included in all *
+ * copies or substantial portions of the Software.                                *
+ *                                                                                *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  *
+ * SOFTWARE.                                                                      *
+ **********************************************************************************/
+
+package com.mohammedsazidalrashid.android.sunshine;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by sazid on 12/19/2014.
+ */
+public class ForecastFragment extends Fragment {
+
+    private final String LOG_TAG = ForecastFragment.class.getSimpleName();
+
+    public ForecastFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        String[] forecastArray = {
+                "Today - Sunny - 88/63",
+                "Tomorrow - Foggy - 74/46",
+                "Weds - Cloudy - 72/63",
+                "Thurs - Rainy - 64/51",
+                "Fri - Foggy - 70/64",
+                "Sat - Sunny - 76/68"
+        };
+
+        List<String> weekForecast = new ArrayList<>(
+                Arrays.asList(forecastArray));
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                getActivity(),
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                weekForecast
+        );
+
+        ListView listviewForecast = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listviewForecast.setAdapter(adapter);
+
+        return rootView;
+    }
+
+    private class FetchWeatherTask extends AsyncTask<String, Void, String> {
+        protected String doInBackground(String... params) {
+            // These two need to be declared outside of the try/catch so that they can be closed
+            // in the finally block
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as String
+            String forecastJsonStr = null;
+
+            try {
+
+                // Construct the url
+                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast" +
+                        "/daily?q=Dhaka,bd&mode=json&units=metric&cnt=7");
+
+                // Open the connection, set its request method and connect to the remote system
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                StringBuffer buffer = new StringBuffer();
+                InputStream inputStream = urlConnection.getInputStream();
+                if (inputStream == null) {
+                    // Nothing to do
+                    return null;
+                } else {
+                    reader = new BufferedReader(new InputStreamReader(inputStream));
+                }
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Adding "\n" for debugging purposes/readability
+                    buffer.append(line).append("\n");
+                }
+
+                if (buffer.length() == 0) {
+                    return null;
+                }
+
+                forecastJsonStr = buffer.toString();
+
+                Log.i(LOG_TAG, "JSON: " + forecastJsonStr);
+
+            } catch (IOException exception) {
+                Log.e(LOG_TAG, "Error ", exception);
+            } catch (NullPointerException exception) {
+                Log.e(LOG_TAG, "Error ", exception);
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException exception) {
+                        Log.e(LOG_TAG, "Error ", exception);
+                    }
+                }
+            }
+
+            return forecastJsonStr;
+        }
+    }
+}
--- Sunshine/app/src/main/java/com/mohammedsazidalrashid/android/sunshine/MainActivity.java
+++ Sunshine/app/src/main/java/com/mohammedsazidalrashid/android/sunshine/MainActivity.java
@@ -26,26 +26,9 @@
 package com.mohammedsazidalrashid.android.sunshine;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -56,7 +39,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -84,99 +67,4 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            // These two need to be declared outside of the try/catch so that they can be closed
-            // in the finally block
-            HttpURLConnection urlConnection = null;
-            BufferedReader reader = null;
-
-            // Will contain the raw JSON response as String
-            String forecastJsonStr = null;
-
-            try {
-                // Construct the url
-                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast" +
-                        "/daily?q=Dhaka,bd&mode=json&units=metric&cnt=7");
-
-                // Open the connection, set its request method and connect to the remote system
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-
-                // Read the input stream into a String
-                StringBuffer buffer = new StringBuffer();
-                InputStream inputStream = urlConnection.getInputStream();
-                if (inputStream == null) {
-                    // Nothing to do
-                    forecastJsonStr = null;
-                } else {
-                    reader = new BufferedReader(new InputStreamReader(inputStream));
-                }
-
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    // Adding "\n" for debugging purposes/readability
-                    buffer.append(line).append("\n");
-                }
-
-                if (buffer.length() == 0) {
-                    forecastJsonStr = null;
-                }
-                forecastJsonStr = buffer.toString();
-                Log.i("PlaceholderFragment", "JSON: " + forecastJsonStr);
-            } catch (IOException exception) {
-                Log.e("PlaceholderFragment", "Error ", exception);
-            } catch (NullPointerException exception) {
-                Log.e("PlaceholderFragment", "Error ", exception);
-            } finally {
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (IOException exception) {
-                        Log.e("PlaceholderFragment", "Error ", exception);
-                    }
-                }
-            }
-
-            String[] forecastArray = {
-                    "Today - Sunny - 88/63",
-                    "Tomorrow - Foggy - 74/46",
-                    "Weds - Cloudy - 72/63",
-                    "Thurs - Rainy - 64/51",
-                    "Fri - Foggy - 70/64",
-                    "Sat - Sunny - 76/68"
-            };
-
-            List<String> weekForecast = new ArrayList<>(
-                    Arrays.asList(forecastArray));
-
-            ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                    getActivity(),
-                    R.layout.list_item_forecast,
-                    R.id.list_item_forecast_textview,
-                    weekForecast
-            );
-
-            ListView listviewForecast = (ListView) rootView.findViewById(R.id.listview_forecast);
-            listviewForecast.setAdapter(adapter);
-
-            return rootView;
-        }
-    }
 }
